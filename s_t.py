4a923aef72e34feb3815c0e948712422b8ef0928
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-03-24 21:20:21 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -17,16 +17,28 @@ st.image(image, width=200)
 
 st.write("¡Bienvenido a CocinaFacil con ChefIA, tu asistente de cocina personal! Aquí podrás narrar tus recetas para que otras personas puedan conocer y disfrutar al máximo de tus creaciones culinarias.")
 
-
 st.write("Toca el botón y cuentanos tu receta")
 
-stt_button = Button(label="Comienza", width=200, button_type="success")
+# CSS for the button
+button_style = """
+    <style>
+        .st-ff {
+            background-color: black !important;
+            color: white !important;
+            border-color: white !important;
+        }
+    </style>
+"""
+
+st.markdown(button_style, unsafe_allow_html=True)
+
+stt_button = Button(label="Comienza", width=200, button_type="success", css_classes=['st-ff'])
 
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
- 
+
     recognition.onresult = function (e) {
         var value = "";
         for (var i = e.resultIndex; i < e.results.length; ++i) {
@@ -57,7 +69,7 @@ if result:
     except:
         pass
     translator = Translator()
-    
+
     text = str(result.get("GET_TEXT"))
     in_lang = st.selectbox(
         "Selecciona el lenguaje de Entrada",
@@ -75,7 +87,7 @@ if result:
         input_language = "zh-cn"
     elif in_lang == "Japonés":
         input_language = "ja"
-    
+
     out_lang = st.selectbox(
         "Selecciona el lenguaje de salida",
         ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
@@ -92,7 +104,7 @@ if result:
         output_language = "zh-cn"
     elif out_lang == "Japonés":
         output_language = "ja"
-    
+
     english_accent = st.selectbox(
         "Selecciona el acento",
         (
@@ -106,12 +118,12 @@ if result:
             "Sudáfrica",
         ),
     )
-    
+
     if english_accent == "Defecto":
         tld = "com"
     elif english_accent == "Español":
         tld = "com.mx"
-    
+
     elif english_accent == "Reino Unido":
         tld = "co.uk"
     elif english_accent == "Estados Unidos":
@@ -124,8 +136,8 @@ if result:
         tld = "ie"
     elif english_accent == "Sudáfrica":
         tld = "co.za"
-    
-    
+
+
     def text_to_speech(input_language, output_language, text, tld):
         translation = translator.translate(text, src=input_language, dest=output_language)
         trans_text = translation.text
@@ -136,22 +148,22 @@ if result:
             my_file_name = "audio"
         tts.save(f"temp/{my_file_name}.mp3")
         return my_file_name, trans_text
-    
-    
+
+
     display_output_text = st.checkbox("Mostrar el texto")
-    
+
     if st.button("Convertir"):
         result, output_text = text_to_speech(input_language, output_language, text, tld)
         audio_file = open(f"temp/{result}.mp3", "rb")
         audio_bytes = audio_file.read()
         st.markdown(f"## Tú audio:")
         st.audio(audio_bytes, format="audio/mp3", start_time=0)
-    
+
         if display_output_text:
             st.markdown(f"## Texto de salida:")
             st.write(f" {output_text}")
-    
-    
+
+
     def remove_files(n):
         mp3_files = glob.glob("temp/*mp3")
         if len(mp3_files) != 0:
